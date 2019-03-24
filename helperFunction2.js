29e1d84a565cb145edffd44eb0633ca77c804451
Christian B <christianbear1221@gmail.com>
2019-03-23 17:30:57 -0700

    backup path - working!

--- helperFunction2.js
+++ helperFunction2.js
@@ -5,6 +5,7 @@ var endArray=[];
 var savedPos = -1;
 var bigArray= [];
 var solidArray = [];
+var backupPathStr = [];
 async function pathLoop(path){
     // spits out next node in tree of paths
 
@@ -18,30 +19,46 @@ async function pathLoop(path){
             arrayOfVal.push(el.val());
             
         });
+        if(arrayOfVal.length==0){
+            var num = path.lastIndexOf("/");
+            backupPathStr= path.substr(0, num);
+            console.log('backupPathStr', backupPathStr);
+        }
         console.log('arrayForPath', arrayForPath);
         console.log('arrayOfVal', arrayOfVal);
+        // makeArray();
     });
     // solidArray = arrayForPath;
 }
 
 var path=[];
 function pathMaker(){
+    console.log('function that combines to make path', );
+    
+    path=[];
     for (let i = 0; i < arrayForPath.length; i++) {
         path.push(arrayForPath[i].o.join('/'));
     }
 }
 
 
-async function makeArray(mainPath){
-await pathLoop(mainPath);
+async function makeArray(){
+// await pathLoop(mainPath);
     solidArray = [];
     for (let i = 0; i < arrayForPath.length; i++) {
         solidArray.push(arrayForPath[i].o.join('/'));
     }
+    console.log('solidArray', solidArray);
 }
 function remove(array, pos){
     array.splice(pos, 1);
 }
+
+function makePathString(previous, toAdd){
+    toAdd = previous + '/'+ toAdd;
+    return toAdd;
+}
+
 //================================================
 var returnArray=[];
 async function getLastElement(mainPath){
@@ -57,8 +74,9 @@ async function getLastElement(mainPath){
 
 var posOfText = -1;
 async function findBasedOnParams(text, param){
-    console.log('function that finds email and returns pos', );
+    console.log('function that finds obj properties and returns pos', );
     await pathLoop('users');
+    pathMaker();
     console.log('path', path);
     for(var i in arrayOfVal){   //arrayOfVal - holds all data
         if(arrayOfVal[i][text]==param){
@@ -69,11 +87,13 @@ async function findBasedOnParams(text, param){
     }
 }
 
+var savedReturnedNode;
 async function lookUpParam(path, param, passedNum){
     console.log('function that returns a node based on <num> in branch and the parameter type {obj}');
     await pathLoop(path);
 
     console.log(arrayOfVal[passedNum][param]);
+    savedReturnedNode = arrayOfVal[passedNum][param];
 }
 
 //================================================
@@ -85,6 +105,34 @@ async function something(){
     }
 }
 
+
+//================================================
+
+async function afterKnowingEmail(myPath, numDeep){
+    console.log('function that pulls Value for a searched and deeper value, serval branches deep');
+
+    path = myPath;
+
+    for(var i=0; i<=numDeep; i++){
+        await pathLoop(path);
+        await pathMaker(); //- makes paths for all child elements
+        await getLastElement(path[0]); //- gets all the child names
+        //chose a child - 
+        console.log('value:', arrayOfVal);
+    }
+
+    // await pathLoop('users');
+    // await pathMaker(); //- makes paths for all child elements
+    // await getLastElement(path[0]); //- gets all the child names
+    // //chose a child - by index
+    // await pathLoop(makePathString(path[index1], returnArray[index2]));
+    // await pathMaker();
+
+    // await getLastElement(path[0]);
+    // console.log('value:', arrayOfVal);
+
+    
+}
 
 //================================================
 var oneBefore = [];
@@ -113,4 +161,10 @@ async function takeInPath(path){
         snap = snapshot.A.B;
         console.log('snap', snap);
     });
+}
+
+//================================================
+
+function savePathBefore(){
+
 }
